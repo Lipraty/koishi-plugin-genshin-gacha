e94d6aeb9b3eb8c52aa3e0bc62b03d62c2d69843
Lipraty <sis1943@outlook.com>
2023-02-17 06:43:36 +0800

    feat: add core function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,42 @@
 import { Context, Schema } from 'koishi'
 
-export const name = 'genshin-gacha'
+type CardLevel = 'S' | 'R' | 'N'
 
-export interface Config {}
+class GenshinGacha {
+  public readonly name = 'genshin-gacha'
+  private probability = {
+    activity: [151, 1282, 8567],
+    weapon: [156, 1498, 8346],
+    permanent: [213, 1387, 8400]
+  } as const
 
-export const Config: Schema<Config> = Schema.object({})
+  constructor(ctx: Context, config: GenshinGacha.Config) {
 
-export function apply(ctx: Context) {
-  // write your plugin here
+  }
+
+  private select(pool: keyof typeof this.probability) {
+    const hit = Math.ceil(Math.random() * 10000 + 1)
+    const _p = this.probability[pool]
+    const _c: CardLevel[] = ['S', 'R', 'N']
+    let start = 0
+    let end = _p.length - 1
+    while (start < end) {
+      let mid = Math.floor((start + end) / 2)
+      if (hit > _p[mid] && hit <= _p[mid + 1]) {
+        return _c[mid + 1]
+      } else if (hit > _p[mid + 1]) {
+        start = mid
+      } else {
+        end = mid
+      }
+      return _c[0]
+    }
+  }
 }
+
+namespace GenshinGacha {
+  export interface Config { }
+  export const Config: Schema<Config> = Schema.object({})
+}
+
+export default GenshinGacha
